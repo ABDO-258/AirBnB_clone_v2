88a4ae9af5a3db97a7b007048260f724a85542d0
abdo <abdoraja2005@gmail.com>
2023-11-20 15:57:24 +0100

    task3  MySQL setup development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,10 @@
+-- script that prepares a MySQL server for the project
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+SET GLOBAL validate_password.policy = 'LOW';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- give priviliges to the MySQL server user hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+SET GLOBAL validate_password.policy = 'MEDIUM';
+
+FLUSH PRIVILEGES;
