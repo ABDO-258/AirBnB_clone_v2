d69fab24816647efc22aea949c46e4208d28c9a1
abdo <abdoraja2005@gmail.com>
2023-11-26 20:05:48 +0100

    task [6]

--- models/state.py
+++ models/state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
 
@@ -9,16 +9,15 @@ from os import getenv
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="delete")
-
     # For DBStorage
     if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state",
                               cascade="all, delete-orphan")
 
     # For FileStorage
     else:
+        name = ""
         @property
         def cities(self):
             from models import storage
