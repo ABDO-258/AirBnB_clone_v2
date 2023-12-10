c9c2d2b79d0f01d2c9be6a8ea355d76799417554
abdo258 <abdoraja2005@gmail.com>
2023-12-10 15:59:07 +0100

    task1 do_pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,7 +3,7 @@
 web_static folder of your AirBnB Clone repo, using the function do_pack"""
 
 from fabric.api import local, task
-import datetime
+from datetime import datetime
 
 
 @task
@@ -13,16 +13,12 @@ def do_pack():
     folder_of_compressed = "versions"
     try:
         # Create the versions folder if it doesn't exist
-        local("mkdir -p versions")
+        #local('mkdir -p "versions"')
         # Create the archive
         now = datetime.utcnow().strftime('%Y%m%d%H%M%S')
         archive_name = f'web_static_{now}.tgz'
-        tar_command = (
-            f'tar -czvf {folder_of_compressed}/{archive_name} '
-            f'{folder_to_compress}'
-        )
+        tar_command = f'tar -czvf {folder_of_compressed}/{archive_name} {folder_to_compress}'
         local(tar_command)
-
         return f'{folder_of_compressed}/{archive_name}'
     except Exception:
         return None
