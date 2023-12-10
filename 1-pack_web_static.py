b8b77a3cadd3a1cac99d55b6f854c6f3650f7acb
abdo258 <abdoraja2005@gmail.com>
2023-12-10 17:39:47 +0100

    task1 do_pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -13,12 +13,14 @@ def do_pack():
     folder_of_compressed = "versions"
     try:
         # Create the versions folder if it doesn't exist
-        #local('mkdir -p "versions"')
+        local('mkdir -p versions')
         # Create the archive
         now = datetime.utcnow().strftime('%Y%m%d%H%M%S')
         archive_name = f'web_static_{now}.tgz'
-        tar_command = f'tar -czvf {folder_of_compressed}/{archive_name} {folder_to_compress}'
+        path = f'{folder_of_compressed}/{archive_name}'
+        tar_command = f'tar -czvf {path} {folder_to_compress}'
         local(tar_command)
-        return f'{folder_of_compressed}/{archive_name}'
+        print(f'Packing web_static to {path}')
+        return (path)
     except Exception:
         return None
