6ea3dd5a4bc64b2c192ce7447f537604eff68609
abdo258 <abdoraja2005@gmail.com>
2023-12-25 11:32:27 +0100

    task8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" flask web app"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def HBNB_states_list():
+    """
+    display a HTML page with the list of all State objects in DBStorage
+    """
+    states = storage.all('State')
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_app():
+    """ remove the current SQLAlchemy Session """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run()
