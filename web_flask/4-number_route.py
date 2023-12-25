912ab4ccace33d56776180abd2e23cf25afa09b8
abdo258 <abdoraja2005@gmail.com>
2023-12-25 01:00:57 +0100

    task4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """ display “Hello HBNB!” """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """ display “HBNB” """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C_is_fun(text):
+    """ 
+        display “C ” followed by the value of the text variable
+        (replace underscore _ symbols with a space )
+    """
+    text_space = text.replace("_", " ")
+    return f"C {text_space}"
+
+
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    """
+    display “Python ”, followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
+    text_space2 = text.replace("_", " ")
+    return f"Python {text_space2}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def python_number(n):
+    """ display “n is a number” only if n is an integer """
+    return f"{n} is a number"
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
